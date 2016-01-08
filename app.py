9b7151a24cb3555068ea2a2920471062b6a7d14a
Ben Lewis <ben@keep.com>
2016-01-08 13:36:44 -0500

    Beginning to configure flask assets & add static file dirs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import logging
 
 import flask
+from flask.ext.assets import Bundle, Environment
 
 from views import BLUEPRINTS
 
@@ -11,10 +12,20 @@ log = logging.getLogger(__name__)
 
 def create_app():
     log.info('Creating Flask app')
+    # Add custom access log
+    logging.getLogger('werkzeug').addHandler(logging.FileHandler('access.log'))
+
     app = flask.Flask(__name__)
+    register_assets(app)
     register_views(app)
     setup_jinja(app)
     return app
+
+
+def register_assets(app):
+    assets = Environment(app)
+    assets.debug = False
+    assets.manifest = 'file'
 
 
 def register_views(app):
